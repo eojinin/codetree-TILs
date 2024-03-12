b04c1d251febfda9683d1a59f67ecf86f443267a
eojinin <163129013+eojinin@users.noreply.github.com>
2024-03-12 21:10:22 +0900

    Title: 특정 위치의 문자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240312/특정 위치의 문자/char-in-specific-location.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char word[6] = { 'L','E','B','R','O','S' };
+	char c;
+	cin >> c;
+
+	for (int i = 0; i < 6; i++) {
+		if (word[i] == c)
+			cout << i;
+	}
+	return 0;
+}
