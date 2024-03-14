03263f9e2fae3082bfafdd6ab708d77e0d5ade0f
eojinin <163129013+eojinin@users.noreply.github.com>
2024-03-14 21:23:16 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240314/배열 놀이/play-with-array.cpp
+++ 240314/배열 놀이/play-with-array.cpp
@@ -10,31 +10,37 @@ int main() {
 		cin >> arr[i];
 	}
 
-	int a, a1, b, a2;
-	int fir, sec, thi;
-	cin >> fir >> a1;
-	cin >> sec >> a2;
-	cin >> thi >> a >> b;
-
-	cout << arr[a1-1] << endl; //1
-
-	int cnt = 0;
-	for (int i = 1; i <= n; i++) { //2
-		if (arr[i-1] == a2) {
-			cnt++;
-			if (cnt > 1)
-				break;
-			cout << i;
-			break;
+	for (int i = 0; i < q; i++) {
+		int q_type;
+		cin >> q_type;
+		if (q_type == 1) {
+			int a;
+			cin >> a;
+			cout << arr[a - 1] << endl; //1
 		}
-	}
-	if (cnt == 0) {
-		cout << "0";
-	}
-
-	cout << endl;
-	for (int i = a; i <= b; i++) {
-		cout << arr[i-1] << " ";
+		else if (q_type == 2) {
+			int a, cnt = 0;
+			cin >> a;
+			for (int i = 1; i <= n; i++) { //2
+				if (arr[i - 1] == a) {
+					cnt++;
+					if (cnt > 1)
+						break;
+					cout << i << endl;
+					break;
+				}
+			}
+			if (cnt == 0) {
+				cout << "0" << endl;
+			}
+		}
+		else if (q_type == 3) {
+			int a, b;
+			cin >> a >> b;
+			for (int i = a; i <= b; i++) {
+				cout << arr[i - 1] << " ";
+			}
+		}
 	}
 	return 0;
 }
